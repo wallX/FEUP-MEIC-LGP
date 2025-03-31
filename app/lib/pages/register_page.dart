6a8925c148d0d6d67dd17779bf28fd616a76815c
Tiago Cruz <tiagocruz057@gmail.com>
2025-03-31 13:43:34 +0100

    seperated build function into various methods

--- app/lib/pages/register_page.dart
+++ app/lib/pages/register_page.dart
@@ -5,7 +5,7 @@ class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
-  _RegisterPageState createState() => _RegisterPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
@@ -21,7 +21,7 @@ class _RegisterPageState extends State<RegisterPage> {
         name: _nameController.text,
         email: _emailController.text,
         userType: _selectedUserType,
-        token: 'example_token',
+        //token: 'example_token',
         stations: _stationsController.text.isNotEmpty
             ? _stationsController.text.split(',')
             : null,
@@ -45,64 +45,84 @@ class _RegisterPageState extends State<RegisterPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextFormField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(labelText: 'Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                ),
+                _buildNameField(),
                 const SizedBox(height: 16),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
+                _buildEmailField(),
                 const SizedBox(height: 16),
-                DropdownButtonFormField<UserType>(
-                  value: _selectedUserType,
-                  decoration: const InputDecoration(labelText: 'User Type'),
-                  items: UserType.values.map((UserType type) {
-                    return DropdownMenuItem<UserType>(
-                      value: type,
-                      child: Text(type.toString().split('.').last),
-                    );
-                  }).toList(),
-                  onChanged: (UserType? newValue) {
-                    setState(() {
-                      _selectedUserType = newValue!;
-                    });
-                  },
-                ),
+                _buildUserTypeDropdown(),
                 const SizedBox(height: 16),
-                TextFormField(
-                  controller: _stationsController,
-                  decoration: const InputDecoration(
-                    labelText: 'Stations (comma-separated)',
-                  ),
-                ),
+                _buildStationsField(),
                 const SizedBox(height: 32),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: _register,
-                    child: const Text('Register'),
-                  ),
-                ),
+                _buildRegisterButton(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildNameField() {
+    return TextFormField(
+      controller: _nameController,
+      decoration: const InputDecoration(labelText: 'Name'),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your name';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      controller: _emailController,
+      decoration: const InputDecoration(labelText: 'Email'),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildUserTypeDropdown() {
+    return DropdownButtonFormField<UserType>(
+      value: _selectedUserType,
+      decoration: const InputDecoration(labelText: 'User Type'),
+      items: UserType.values.map((UserType type) {
+        return DropdownMenuItem<UserType>(
+          value: type,
+          child: Text(type.toString().split('.').last),
+        );
+      }).toList(),
+      onChanged: (UserType? newValue) {
+        setState(() {
+          _selectedUserType = newValue!;
+        });
+      },
+    );
+  }
+
+  Widget _buildStationsField() {
+    return TextFormField(
+      controller: _stationsController,
+      decoration: const InputDecoration(
+        labelText: 'Stations (comma-separated)',
+      ),
+    );
+  }
+
+  Widget _buildRegisterButton() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: _register,
+        child: const Text('Register'),
       ),
     );
   }
