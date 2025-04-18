7dc6713b9d6dcf712cd07e8d1218c94370a77ae3
Tiago Cruz <tiagocruz057@gmail.com>
2025-04-18 17:01:26 +0100

    Adding new files doesn't replace the old ones

--- app/lib/pages/submission_page.dart
+++ app/lib/pages/submission_page.dart
@@ -103,7 +103,7 @@ class _SubmissionPageState extends State<SubmissionPage> {
 
     if (mediaFiles != null) {
       List<File> files = mediaFiles.paths.map((path) => File(path!)).toList();
-      List<CustomFile> customFiles = [];
+      List<CustomFile> customFiles = _submissionProvider.selectedFiles;
 
       for (File file in files) {
         final metadata = await _extractVideoMetadata(file.path);
--- app/lib/provider/submission_provider.dart
+++ app/lib/provider/submission_provider.dart
@@ -4,17 +4,27 @@ import 'package:app/data/custom_file.dart';
 /// Class to manage the submission of videos in the app, so that by leaving the submission_page, videos won't be removed
 class SubmissionProvider extends ChangeNotifier {
   List<CustomFile> selectedFiles = [];
+  //Set<String> selectedFilesSet = {};
   bool isUploading = false;
-  
-  void setSelectedFiles(List<CustomFile> files) {
+
+  void setFiles(List<CustomFile> files) {
     selectedFiles = files;
     notifyListeners();
   }
-  
-  void addFile(CustomFile file) {
-    selectedFiles.add(file);
+
+  void addFiles(List<CustomFile> files) {
+    selectedFiles.addAll(files);
     notifyListeners();
   }
+
+  /*bool addPathToSet(String path) {
+    if (selectedFilesSet.contains(path)) {
+      return false; // Path already exists in the set
+    }
+    selectedFilesSet.add(path);
+    notifyListeners();
+    return true; // Path added successfully
+  }*/
   
   void removeFile(int index) {
     if (index >= 0 && index < selectedFiles.length) {
@@ -25,6 +35,7 @@ class SubmissionProvider extends ChangeNotifier {
   
   void clearFiles() {
     selectedFiles.clear();
+    //selectedFilesSet.clear();
     notifyListeners();
   }
   
